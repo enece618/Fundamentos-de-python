05c9045d490f3c0ad17b6239a61be2a978a995fd
Fernando Nevarez <49252590+enece618@users.noreply.github.com>
2023-04-14 20:31:01 -0600

    add the practice to the repository

---
+++ FundamentosPython.py
@@ -0,0 +1,38 @@
+print("-------- Operadores Aritméticos --------")
+print("Operador suma, 5 + 3: ", 5 + 3)
+print("Operador resta, 5 - 3: ", 5 - 3)
+print("Operador multiplicación, 5 * 3: ", 5 * 3)
+print("Operador división, 5 / 3: ", 5 / 3)
+print("Operador división entera, 5 // 3: ", 5 // 3)
+print("Operador módulo, 5 % 3: ", 5 % 3)
+print("Operador cambio de signo,  -3: ", -3)
+print("Operador exponente,  5 ** 5: ", 5 ** 5)
+
+# Este es un comentario de una línea
+'''Este es un comentario
+de varias líneas '''
+print("\n-------- Operadores Lógicos --------\n")
+
+print("\n-------- Operador AND --------\n")
+print("True and True: ", True and True)
+print("True and False: ", True and False)
+print("False and True: ", False and True)
+print("False and False: ", False and False)
+
+print("\n-------- Operador OR --------\n")
+print("True or True: ", True or True)
+print("True or False: ", True or False)
+print("False or True: ", False or True)
+print("False or False: ", False or False)
+
+print("\n-------- Operador NOT --------\n")
+print("True: ", not True)
+print("False: ", not False)
+
+print("\n-------- Operadores de comparación --------\n")
+print("==: ", 1 == 1)
+print("!=: ", 1 != 2)
+print("<: ", 1 < 2)
+print("<=: ", 1 <= 2)
+print(">: ", 1 > 2)
+print(">=: ", 1 >= 2)
